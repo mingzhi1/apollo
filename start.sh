3d4be4b52d77b83174f86f2da37a3604ef3655df
ming <945012257@qq.com>
2018-05-29 16:46:57 +0800

    sh

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-sh scripts/build.sh
+read -p "是否跳过打包?"  skip
+if [[  $skip == y ]] || [[  $skip == Y ]];then
+		echo "已跳过，开始执行容器"
+	else
+		sh scripts/build.sh
+	fi
 cd apollo-portal/
-sh apollo-portal/runDocker.sh
+sh runDocker.sh
 cd -
 cd apollo-configservice/
-sh apollo-configservice/runDocker.sh
+sh runDocker.sh
 cd -
 cd apollo-adminservice/
-sh apollo-adminservice/runDocker.sh
+sh runDocker.sh
