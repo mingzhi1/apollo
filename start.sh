265a352a9006c8d5ea727c44b472a286cb5e1c5c
ming <945012257@qq.com>
2018-05-29 16:40:21 +0800

    sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 sh scripts/build.sh
+cd apollo-portal/
 sh apollo-portal/runDocker.sh
+cd -
+cd apollo-configservice/
 sh apollo-configservice/runDocker.sh
+cd -
+cd apollo-adminservice/
 sh apollo-adminservice/runDocker.sh
