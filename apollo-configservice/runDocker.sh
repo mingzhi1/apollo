2846fecdb7585459b4cbe1c2edaac6681004a6aa
ming <945012257@qq.com>
2018-06-13 15:24:57 +0800

    docker name

--- apollo-configservice/runDocker.sh
+++ apollo-configservice/runDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t apollo-configservice .
 docker rm -f apollo-configservice
-docker run -p 8051:8051 -d --name apollo-configservice -v /data/apollo/apollo-log/100003171:/opt/logs/100003171 apollo-configservice-dev
+docker run -p 8051:8051 -d --name apollo-configservice-dev -v /data/apollo/apollo-log/100003171:/opt/logs/100003171 apollo-configservice
